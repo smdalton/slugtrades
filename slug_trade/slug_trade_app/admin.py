d7afae432f027da042041646e728191e165afafd
smd <smdalton@ucsc.edu>
2018-04-07 12:21:53 -0700

    whole directory

--- slug_trade/slug_trade_app/admin.py
+++ slug_trade/slug_trade_app/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from .models import Profile, Item, Picture, Offer
+
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     list_display = ('')
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
 
 # Register your models here.
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
+admin.site.register(Item)
+admin.site.register(Picture)
+admin.site.register(Offer)
