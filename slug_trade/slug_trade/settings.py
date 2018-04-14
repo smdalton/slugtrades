41ddf7720c058adb5b0676e70fb56fcfbc2deb12
Austin Kellar <ajk@Austins-MacBook-Pro.local>
2018-04-14 14:19:28 -0700

    first pass at simple login logic

--- slug_trade/slug_trade/settings.py
+++ slug_trade/slug_trade/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+LOGIN_REDIRECT_URL = '/slug_trade_app/home/'
--- slug_trade/slug_trade_app/urls.py
+++ slug_trade/slug_trade_app/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from slug_trade_app import views
+from django.contrib.auth import views as auth_views
 
 #template tagging
 app_name = 'slug_trade_app'
@@ -7,4 +8,6 @@ app_name = 'slug_trade_app'
 urlpatterns = [
     url(r'^home/$', views.index, name='index'),
     url(r'^products/$', views.products, name='products'),
+    url(r'^login/$', auth_views.login, {'template_name': 'slug_trade_app/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'slug_trade_app/logout.html'}, name='logout')
 ]
