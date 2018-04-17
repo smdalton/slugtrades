3808f8109ffc58ae140379b40b16b8714e0045fb
Austin Kellar <ajk@Austins-MacBook-Pro.local>
2018-04-17 14:00:36 -0700

    first pass at edit profile functionality

--- slug_trade/slug_trade_app/views.py
+++ slug_trade/slug_trade_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from . import models
+from django.http import HttpResponseRedirect
+import json
 # Create your views here.
 
 def index(request):
@@ -20,3 +22,22 @@ def profile(request):
         return render(request, 'slug_trade_app/profile.html', {'user': request.user})
     else:
         return render(request, 'slug_trade_app/not_authenticated.html')
+
+def edit_profile(request):
+    if request.method == 'POST':
+        user = User.objects.get(id=request.user.id)
+        data = request.POST
+
+        user.first_name = data['first_name']
+        user.last_name = data['last_name']
+        user.userprofile.bio = data['bio']
+        user.userprofile.on_off_campus = data['on_off_campus']
+        user.userprofile.profile_picture = request.FILES['profile_picture']
+        user.save()
+
+        return HttpResponseRedirect('/slug_trade_app/profile/')
+    else:
+        if request.user.is_authenticated():
+            return render(request, 'slug_trade_app/edit_profile.html', {'user': request.user})
+        else:
+            return render(request, 'slug_trade_app/not_authenticated.html')
