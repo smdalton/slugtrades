fec9df3f9f719422194a52ece984d5fcdd4a6cca
Sidney Eubanks <onelookingformusic@gmail.com>
2018-04-19 10:36:30 -0700

    adding working signup form without second model

--- slug_trade/slug_trade_app/forms.py
+++ slug_trade/slug_trade_app/forms.py
@@ -1,16 +1,62 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from slug_trade_app.models import UserProfile
 
-class UserForm(forms.ModelForm):
-    
-    password = forms.CharField(widget=forms.PasswordInput())
+class UserForm(UserCreationForm):
+    email = forms.EmailField(required=False)
 
-    class Meta():
+    class Meta:
         model = User
-        fields = ('username','first_name', 'last_name', 'email', 'password')
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2',
+        )
+
+    def save(self, commit=True):
+        user = super(UserForm, self).save(commit=False)
+        user.username = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+
+        return user
+
 
 class UserProfileForm(forms.ModelForm):
     class Meta():
         model = UserProfile
         fields = ('profile_picture', 'bio', 'on_off_campus')
+
+
+
+
+
+
+
+
+
+
+
+# from django import forms
+# from django.contrib.auth.models import User
+# from slug_trade_app.models import UserProfile
+#
+# class UserForm(forms.ModelForm):
+#
+#     password = forms.CharField(widget=forms.PasswordInput())
+#
+#     class Meta():
+#         model = User
+#         fields = ('username','first_name', 'last_name', 'email', 'password')
+#
+# class UserProfileForm(forms.ModelForm):
+#     class Meta():
+#         model = UserProfile
+#         fields = ('profile_picture', 'bio', 'on_off_campus')
--- slug_trade/slug_trade_app/views.py
+++ slug_trade/slug_trade_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from slug_trade_app.forms import UserForm, UserProfileForm
 from . import models
@@ -21,40 +22,18 @@ def profile(request):
         return render(request, 'slug_trade_app/not_authenticated.html')
 
 def signup(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
 
-    signed_in = False
-
-    if request.method == "POST":
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(request.POST)
-
-        print(user_form.is_valid())
-        print(profile_form.is_valid())
-
-        if user_form.is_valid() and profile_form.is_valid():
-
+        if user_form.is_valid():
             user = user_form.save()
-            user.set_password(user.password)
-            user.save()
 
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            if 'profile_picture' in request.FILES:
-                profile.profile_picture = request.FILES['profile_picture']
-
-            profile.save()
-
-            signed_in = True
-
-        else:
-            print(user_form.errors, profile_form.errors)
-
+            user = authenticate(username=user_form.cleaned_data['email'],
+                                    password=user_form.cleaned_data['password1'],
+                                    )
+            login(request, user)
+            return redirect('/home')
     else:
         user_form = UserForm()
-        profile_form = UserProfileForm()
 
-    return render(request, 'slug_trade_app/signup.html',
-                            {'user_form':user_form,
-                             'profile_form':profile_form,
-                             'signed_in':signed_in})
+        return render(request, 'slug_trade_app/signup.html', {'user_form': user_form})
