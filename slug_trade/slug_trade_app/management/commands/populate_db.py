d7aa3cf0ffc853b6903bf2b6e4f24b4a6da6186e
smd <smdalton@ucsc.edu>
2018-04-19 12:13:59 -0700

    creates single admin account

--- slug_trade/slug_trade_app/management/commands/populate_db.py
+++ slug_trade/slug_trade_app/management/commands/populate_db.py
@@ -2,7 +2,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from django.core.management import call_command
-from slug_trade_app import models
+# from slug_trade_app import models
 
 from faker import Faker
 import random
@@ -18,7 +18,7 @@ class Command(BaseCommand):
     random.seed(4321)
     num_users = 10
     profile_data = [(fake.name(), fake.text(), random.choice(['on', 'off'])) for counter in range(num_users)]
-
+    relative_dir = 'slug_trade/media/db_populate/profile_pics'
         # Todo get the photos directory
     help = 'This is a customized command accessible from manage.py'
     print('calling populate db with  manage.py')
@@ -38,28 +38,41 @@ class Command(BaseCommand):
         user.last_name = 'istrator'
         # userprofile fields
         user.save()
-
         # Operations
-        user.userprofile.bio = 'This is a test bio for admin'
+        user.userprofile.bio = 'This is a test bio for admin, I chose this photo because this is what I feel like inside\n' \
+                               'after having been in college for like 9 years'
         # TODO://
         counter = 0
         # When looking at the project from the point of view of your system's root directory
-        pwd = os.getcwd()
         # when looking in from top level of project, this is where the profile pics are at
-        relative_dir = 'slug_trade/media/db_populate/profile_pics'
+
         # pick a specific
-        filename = os.listdir(os.path.join(pwd, relative_dir))[counter]
+        filename = os.listdir(os.path.join(os.getcwd(), self.relative_dir))[counter]
         user.userprofile.on_off_campus = random.choice(['on', 'off'])
-        full_path = os.path.join(pwd, relative_dir, filename)
+        full_path = os.path.join(os.getcwd(), self.relative_dir, filename)
         print(full_path)
         user.userprofile.profile_picture.save(user.first_name, File(open(full_path, 'rb')))
         user.userprofile.save()
         return
 
-    def create_single_user(self):
-
-        user = User.objects.create_user('')
+    def create_single_user(self, information):
+        user = User.objects.create_user(information[0])
         return
+
+    def create_users(self):
+
+        for name, text, location in self.profile_data:
+            user = User.objects.create_user(name.split(' ')[0], 'pass1234')
+            user.is_superuser = False
+            user.is_staff = False
+            user.first_name = name.split(' ')[0]
+            user.last_name = name.split(' ')[1]
+            user.save()
+
+            user.userprofile.bio = fake.text()
+
+
+    # utility
     def dir_print(self):
         print("\n\n ===========current Dir is {0} ===========\n".format(
             os.getcwd()
@@ -73,12 +86,9 @@ class Command(BaseCommand):
         profile = os.listdir(os.getcwd())
         print("files for profile pics", profile[0])
 
-
-
     def wipe_db(self):
         call_command('flush')
-        sleep(.3)
-        sys.stdout.write('yes\n')
+        self.stdout.write('DB has been flushed')
 
     def create_single_item(self):
         return
@@ -93,6 +103,6 @@ class Command(BaseCommand):
         else:
             self.wipe_db()
             self.create_admin()
-            self.stdout.write('This worked, you nuked the db and started over')
+            self.create_users()
             self.stdout.write('flushed')
 
