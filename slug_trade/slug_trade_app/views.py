7a877d194d5f1c45420a2efc2b89be44f55a9d25
Austin Kellar <ajk@Austins-MacBook-Pro.local>
2018-04-18 20:15:43 -0700

    image validation and urls that match Sidney's change

--- slug_trade/slug_trade_app/views.py
+++ slug_trade/slug_trade_app/views.py
@@ -23,17 +23,15 @@ def edit_profile(request):
     if request.method == 'POST':
         user = User.objects.get(id=request.user.id)
         data = request.POST
-
         user.first_name = data['first_name']
         user.last_name = data['last_name']
         user.userprofile.bio = data['bio']
         user.userprofile.on_off_campus = data['on_off_campus']
         user.userprofile.profile_picture = request.FILES['profile_picture']
         user.save()
-
         return HttpResponseRedirect('/slug_trade_app/profile/')
     else:
         if request.user.is_authenticated():
-            return render(request, 'slug_trade_app/edit_profile.html', {'user': request.user})
+            return render(request, 'slug_trade_app/edit_profile.html', {'user': request.user, 'no_pic': False})
         else:
             return render(request, 'slug_trade_app/not_authenticated.html')
