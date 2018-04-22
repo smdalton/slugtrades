fbe82ff4554236a58079daadddb0d1d084bbfb5f
Sidney Eubanks <onelookingformusic@gmail.com>
2018-04-22 15:27:07 -0700

    commiting two different ways to implement sign up for checking

--- slug_trade/slug_trade_app/forms.py
+++ slug_trade/slug_trade_app/forms.py
@@ -4,8 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from slug_trade_app.models import UserProfile
 
 class UserForm(UserCreationForm):
-    email = forms.EmailField(required=False)
-
+    # email = forms.EmailField(required=False)
     class Meta:
         model = User
         fields = (
@@ -16,17 +15,17 @@ class UserForm(UserCreationForm):
             'password2',
         )
 
-    def save(self, commit=True):
-        user = super(UserForm, self).save(commit=False)
-        user.username = self.cleaned_data['email']
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.email = self.cleaned_data['email']
-
-        if commit:
-            user.save()
-
-        return user
+    # def save(self, commit=True):
+    #     user = super(UserForm, self).save(commit=False)
+    #     user.username = self.cleaned_data['email']
+    #     user.first_name = self.cleaned_data['first_name']
+    #     user.last_name = self.cleaned_data['last_name']
+    #     user.email = self.cleaned_data['email']
+    #
+    #     if commit:
+    #         user.save()
+    #
+    #     return user
 
 
 class UserProfileForm(forms.ModelForm):
--- slug_trade/slug_trade_app/views.py
+++ slug_trade/slug_trade_app/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from slug_trade_app.forms import UserForm, UserProfileForm
 from . import models
+from slug_trade_app.models import UserProfile
 # Create your views here.
 
 def index(request):
@@ -22,11 +23,52 @@ def profile(request):
         return render(request, 'slug_trade_app/not_authenticated.html')
 
 def signup(request):
+
     if request.method == 'POST':
+
         user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
 
         if user_form.is_valid():
-            user = user_form.save()
+            print("1")
+            #user
+            created_user = user_form.save(commit=False)
+            created_user.username = created_user.email
+            created_user.set_password(created_user.password)
+            created_user.save()
+            print("2")
+
+            #extended profile
+            created_profile = profile_form.save(commit=False)
+            print("3")
+            created_profile.user = created_user
+            print("4")
+            created_profile.save()
+            print("5")
+
+
+            # # create user
+            # username = request.POST['email']
+            # first_name = request.POST['first_name']
+            # last_name = request.POST['last_name']
+            # email = request.POST['email']
+            # password = request.POST['password1']
+            #
+            # user = User.objects.create_user(username=username,
+            #                     first_name=first_name,
+            #                     last_name=last_name,
+            #                     email=email,
+            #                     password=password)
+            #
+            # #extended
+            # bio = request.POST['bio']
+            # on_off_campus = request.POST['on_off_campus']
+            #
+            # user_profile = UserProfile.objects.create( user=user,
+            #                     bio=bio,
+            #                     on_off_campus=on_off_campus)
+
+
 
             user = authenticate(username=user_form.cleaned_data['email'],
                                     password=user_form.cleaned_data['password1'],
@@ -35,5 +77,6 @@ def signup(request):
             return redirect('/home')
     else:
         user_form = UserForm()
+        profile_form = UserProfileForm()
 
-        return render(request, 'slug_trade_app/signup.html', {'user_form': user_form})
+        return render(request, 'slug_trade_app/signup.html', {'user_form': user_form, 'profile_form': profile_form})
