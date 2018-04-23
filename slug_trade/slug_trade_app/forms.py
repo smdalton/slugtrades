eef6ad6b6f06f9298dae22886ed99bcf0a60a12c
Sidney Eubanks <onelookingformusic@gmail.com>
2018-04-22 23:50:50 -0700

    working signup form with validation checks. Still working on image part

--- slug_trade/slug_trade_app/forms.py
+++ slug_trade/slug_trade_app/forms.py
@@ -4,10 +4,51 @@ from django.contrib.auth.forms import UserCreationForm
 from slug_trade_app.models import UserProfile
 
 class UserForm(UserCreationForm):
-    # email = forms.EmailField(required=False)
+
+    username = forms.CharField(
+        error_messages={'required': 'Username is required', 'max_length': 'Username name too long'}, required=True,
+        max_length=256,
+        widget=forms.TextInput(attrs={'class': 'form-input', 'id': 'username', 'name': 'fname', 'value' : 'MyCo', 'placeholder': 'Username','autocomplete':'given-name'}))
+
+    first_name = forms.CharField(
+        error_messages={'required': 'First name is required', 'max_length': 'First name too long'}, required=True,
+        max_length=256,
+        widget=forms.TextInput(attrs={'class': 'form-input', 'name': 'fname','placeholder': 'First Name','autocomplete':'given-name'}))
+
+    last_name = forms.CharField(
+        error_messages={'required': 'Last name is required', 'max_length': 'Last name too long'}, required=True,
+        max_length=256,
+        widget=forms.TextInput(attrs={'class': 'form-input','name': 'fname','placeholder': 'Last Name','autocomplete':'family-name'}))
+
+    email = forms.CharField(
+        error_messages={'required': 'Email address is required', 'max_length': 'Email is too long'}, required=True,
+        max_length=256,
+        widget=forms.TextInput(attrs={'class': 'form-input','name':'email','placeholder':'Email Address','autocomplete':'email'}))
+
+    password1 = forms.CharField(
+        error_messages={'required': 'Password required', 'max_length': 'Password is too long'}, required=True,
+        max_length=256,
+        widget=forms.TextInput(attrs={'class': 'form-input','name':'password','placeholder':'Password','autocomplete':'password'}))
+
+    password2 = forms.CharField(
+        error_messages={'required': 'Verification password required', 'max_length': 'Password is too long'}, required=True,
+        max_length=256,
+        widget=forms.TextInput(attrs={'class': 'form-input','name':'password','placeholder':'Verification Password'}))
+
+    def clean_email(self):
+        username = self.cleaned_data["email"]
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError("An account is already active with that email.")
+
+
     class Meta:
         model = User
+        # exclude = ('username',)
         fields = (
+            'username',
             'first_name',
             'last_name',
             'email',
@@ -15,47 +56,12 @@ class UserForm(UserCreationForm):
             'password2',
         )
 
-    # def save(self, commit=True):
-    #     user = super(UserForm, self).save(commit=False)
-    #     user.username = self.cleaned_data['email']
-    #     user.first_name = self.cleaned_data['first_name']
-    #     user.last_name = self.cleaned_data['last_name']
-    #     user.email = self.cleaned_data['email']
-    #
-    #     if commit:
-    #         user.save()
-    #
-    #     return user
-
 
 class UserProfileForm(forms.ModelForm):
     class Meta():
         model = UserProfile
-        fields = ('profile_picture', 'bio', 'on_off_campus')
-
-
-
-
-
-
-
-
-
-
-
-# from django import forms
-# from django.contrib.auth.models import User
-# from slug_trade_app.models import UserProfile
-#
-# class UserForm(forms.ModelForm):
-#
-#     password = forms.CharField(widget=forms.PasswordInput())
-#
-#     class Meta():
-#         model = User
-#         fields = ('username','first_name', 'last_name', 'email', 'password')
-#
-# class UserProfileForm(forms.ModelForm):
-#     class Meta():
-#         model = UserProfile
-#         fields = ('profile_picture', 'bio', 'on_off_campus')
+        fields = (
+            'profile_picture',
+            'bio',
+            'on_off_campus'
+        )
--- slug_trade/slug_trade_app/views.py
+++ slug_trade/slug_trade_app/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from slug_trade_app.forms import UserForm, UserProfileForm
 from . import models
 from slug_trade_app.models import UserProfile
+
+
 # Create your views here.
 
 def index(request):
@@ -30,51 +32,40 @@ def signup(request):
         profile_form = UserProfileForm(request.POST)
 
         if user_form.is_valid():
-            print("1")
-            #user
-            created_user = user_form.save(commit=False)
-            created_user.username = created_user.email
-            created_user.set_password(created_user.password)
-            created_user.save()
-            print("2")
-
-            #extended profile
-            created_profile = profile_form.save(commit=False)
             print("3")
+
+            #create user
+            created_user = user_form.save()
+
+            created_user.username = created_user.email
+            created_user.set_password(user_form.cleaned_data.get('password1'))
+            created_user.save()
+
+            #create extended profile
+            created_profile = profile_form.save(commit=False)
             created_profile.user = created_user
-            print("4")
+            profile.profile_picture = request.FILES['profile_picture']
             created_profile.save()
-            print("5")
 
+            #authentication
+            email = user_form.cleaned_data.get('email')
+            password = user_form.cleaned_data.get('password1')
 
-            # # create user
-            # username = request.POST['email']
-            # first_name = request.POST['first_name']
-            # last_name = request.POST['last_name']
-            # email = request.POST['email']
-            # password = request.POST['password1']
-            #
-            # user = User.objects.create_user(username=username,
-            #                     first_name=first_name,
-            #                     last_name=last_name,
-            #                     email=email,
-            #                     password=password)
-            #
-            # #extended
-            # bio = request.POST['bio']
-            # on_off_campus = request.POST['on_off_campus']
-            #
-            # user_profile = UserProfile.objects.create( user=user,
-            #                     bio=bio,
-            #                     on_off_campus=on_off_campus)
+            authenticated = authenticate(
+                username=email,
+                password=password
+            )
+            #if user is authenticated log them in and redirect
+            if authenticated:
+                login(request, authenticated)
+                return redirect('/home')
+            else:
+                print("not authenticated")
+                return redirect('/home')
+        else:
+            print("NOT VALID")
+            return render(request, 'slug_trade_app/signup.html', {'user_form': user_form, 'profile_form': profile_form})
 
-
-
-            user = authenticate(username=user_form.cleaned_data['email'],
-                                    password=user_form.cleaned_data['password1'],
-                                    )
-            login(request, user)
-            return redirect('/home')
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
