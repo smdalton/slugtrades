98ff2faeaf6bae23b858bd7ddbbf208ac72288fc
smd <smdalton@ucsc.edu>
2018-05-16 13:38:56 -0700

    altered schema for max-length

--- slug_trade/slug_trade_app/models.py
+++ slug_trade/slug_trade_app/models.py
@@ -70,7 +70,7 @@ class Item(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=80, blank=False)
     price = models.DecimalField(max_digits=9, decimal_places=2, blank=False)
-    category = models.CharField(max_length=1,
+    category = models.CharField(max_length=8,
                                 default="O",
                                 choices=ITEM_CATEGORIES,
                                 blank=False)
